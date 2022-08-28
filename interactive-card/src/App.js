f1ae220000fb29f51d1768de1195b60451b6261f
neeraj <neerajp4321@gmail.com>
2022-08-28 18:36:33 +0530

    initialize

--- interactive-card/src/App.js
+++ interactive-card/src/App.js
@@ -3,7 +3,36 @@ import React
 function App() {
   return (
     <div className="App">
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>.
+        <div>
+          Coded by <a href="#">Your Name Here</a>.
+          0000 0000 0000 0000
+          Jane Appleseed
+          00/00
 
+          000
+
+          Cardholder Name
+          e.g. Jane Appleseed
+
+          Card Number
+          e.g. 1234 5678 9123 0000
+
+          Exp. Date (MM/YY)
+          MM
+          YY
+
+          CVC
+          e.g. 123
+
+          Confirm
+
+          Thank you!
+          We've added your card details
+          Continue
+        </div>
+      </div>
     </div>
   );
 }
