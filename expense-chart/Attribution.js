d35e79ebba1dae9295d08cdec19541cb2d2e4082
neeraj <neerajp4321@gmail.com>
2022-09-03 17:02:01 +0530

    commit

---
+++ expense-chart/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Attribution() {
+  return (
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/nkp1111/">Your Name Here</a>.
+    </div>
+  )
+}
+
+export default Attribution
---
+++ expense-chart/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+/**  My balance
+  $921.48
+
+  Spending - Last 7 days
+
+  mon
+  tue
+  wed
+  thu
+  fri
+  sat
+  sun
+
+  Total this month
+  $478.33
+
+  +2.4%
+  from last month
+  
+   */
