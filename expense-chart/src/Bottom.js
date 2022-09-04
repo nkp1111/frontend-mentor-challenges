9d910091da024b769615e23db5cbf6447beffbe0
neeraj <neerajp4321@gmail.com>
2022-09-04 09:25:35 +0530

    commit

---
+++ expense-chart/src/Bottom.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Bottom() {
+  return (
+    <div id='bottom'>
+      <p>Total this month</p>
+      <div>
+        <div>
+          <p>$478.33</p>
+        </div>
+        <div>
+          <p>+2.4%</p>
+          <p>from last month</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Bottom
+/**  
+  
+
+  mon
+  tue
+  wed
+  thu
+  fri
+  sat
+  sun
+
+  
+  
+
+  
+  
+  
+   */
--- expense-chart/src/Main.js
+++ expense-chart/src/Main.js
@@ -1,31 +1,16 @@
 import React from 'react'
 import SVG from './SVG'
+import Bottom from './Bottom'
 
 function Main() {
   return (
     <main>
       <h2>Spending - Last 7 days</h2>
       <SVG />
+      <div className='underline' />
+      <Bottom />
     </main>
   )
 }
 
 export default Main
-/**  
-  
-
-  mon
-  tue
-  wed
-  thu
-  fri
-  sat
-  sun
-
-  Total this month
-  $478.33
-
-  +2.4%
-  from last month
-  
-   */
