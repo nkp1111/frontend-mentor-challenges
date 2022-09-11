7737235509ce74ca8b7e24c14d6a7ecb0c56f751
neeraj <neerajp4321@gmail.com>
2022-09-11 12:58:48 +0530

    design

--- dropdown-menu/src/Header/index.js
+++ dropdown-menu/src/Header/index.js
@@ -1,27 +1,43 @@
 import React from 'react'
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs'
 
 function Header() {
+  const MediumGray = 'hsl(0, 0%, 41%)'
+  const AlmostBlack = 'hsl(0, 0%, 8%)'
+  const color = MediumGray
   return (
     <header className="App-header
-    d-flex">
-      <div>
+    d-flex
+    justify-content-start
+    align-items-center
+    "
+      style={{ color: color }}>
+      <div >
         <img src='https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b4dcf216922bb6f28428d89fd83e06d19a16bc04/dropdown-menu/public/images/logo.svg' className="App-logo" alt="logo" />
       </div>
-
-      <div>
-        <nav>
-          <ul>
-            <li>Features BsChevronDown BsChevronUp</li>
-            <li>Company</li>
-            <li>Careers</li>
-            <li>About</li>
-          </ul>
-        </nav>
-      </div>
-
-      <div>
-        <p>Login</p>
-        <button>Register</button>
+      <nav>
+        <ul
+          className='d-flex
+        justify-space-between
+        align-items-center
+        mt-2
+        '>
+          <li>Features&nbsp;<BsChevronDown /> </li>
+          <li>Company&nbsp;<BsChevronUp /></li>
+          <li>Careers</li>
+          <li>About</li>
+        </ul>
+      </nav>
+      <div
+        className='d-flex
+      justify-content-end
+      align-items-center
+      '>
+        <p
+          className='my-auto'>
+          Login</p>
+        <button
+        >Register</button>
       </div>
     </header>
   )
--- dropdown-menu/src/Main/index.js
+++ dropdown-menu/src/Main/index.js
@@ -1,24 +1,39 @@
 import React from 'react'
 
 function index() {
+
   return (
-    <main>
-      <section>
-        <article>
-          <h1>Make remote work</h1>
-          <p>  Get your team in sync, no matter your location.Streamline processes,
-            create team rituals, and watch productivity soar.</p>
-          <button>Learn more</button>
+    <main className='d-flex
+    justify-content-evenly
+    w-100'>
+      <section
+        className='w-50
+        '>
+        <article className='d-flex
+        flex-column
+        justify-content-evenly'>
+          <h1>Make<br /> remote work</h1>
+          <div id='text-holder'>
+            <p>Get your team in sync, no matter your location. Streamline processes,
+              create team rituals, and watch productivity soar.</p>
+          </div>
+          <div className='w-50 p-0'>
+            <button>Learn more</button>
+          </div>
         </article>
-        <aside>
+        <aside className='d-flex
+        justify-content-between'>
           <img src='https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b4dcf216922bb6f28428d89fd83e06d19a16bc04/dropdown-menu/public/images/client-databiz.svg' alt='databiz-logo' />
           <img src='https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b4dcf216922bb6f28428d89fd83e06d19a16bc04/dropdown-menu/public/images/client-audiophile.svg' alt='audio-phile-logo' />
           <img src='https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b4dcf216922bb6f28428d89fd83e06d19a16bc04/dropdown-menu/public/images/client-meet.svg' alt='meet-logo' />
           <img src='https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b4dcf216922bb6f28428d89fd83e06d19a16bc04/dropdown-menu/public/images/client-maker.svg' alt='maker-logo' />
         </aside>
       </section>
-      <section>
-        <img src='https://github.com/nkp1111/frontend-mentor-challenges/blob/main/dropdown-menu/public/images/image-hero-desktop.png?raw=true' alt='black background with man using laptop' />
+      <section className='d-flex
+      justify-content-end'>
+        <img src='https://github.com/nkp1111/frontend-mentor-challenges/blob/main/dropdown-menu/public/images/image-hero-desktop.png?raw=true'
+          alt='black background with man using laptop'
+          className='w-100' />
       </section>
     </main>
   )
