9fafd440a3828982f8186c6fdc51de5ee606ac45
neeraj <neerajp4321@gmail.com>
2022-09-24 11:23:29 +0530

    Add main and header

--- notification-page/src/App.js
+++ notification-page/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './Header/index'
+import Main from './Main/index'
 import Footer from './Footer/index'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Main />
       <Footer />
     </div>
   );
---
+++ notification-page/src/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <header class="text-center opacity-0">
+      <h1>Notification Page</h1>
+    </header>
+  )
+}
+
+export default index
