f2cc1755d85ca1ee83be7b165b8a396b830342ef
neeraj <neerajp4321@gmail.com>
2022-10-01 16:08:33 +0530

    Feat: add home text

--- space_tourism_website/src/Home.js
+++ space_tourism_website/src/Home.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
+      <p>So, you want to travel to</p>
+      <p>Space</p>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely go to
+        outer space and not hover kind of on the edge of it. Well sit back, and relax
+        because we’ll give you a truly out of this world experience!
+      </p>
 
+      <div>Explore</div>
     </div>
   )
 }
