78239d34a823c532d4e712c9a2aeda2d8c9524a0
neeraj <neerajp4321@gmail.com>
2022-10-01 18:29:11 +0530

    Feat: add router in app component

--- space_tourism_website/src/App.js
+++ space_tourism_website/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Home from './Home'
 import Header from './Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Home />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
