8133a36000d803cfb9b17ab048ae871c71a3e550
neeraj <neerajp4321@gmail.com>
2022-10-01 20:14:27 +0530

    Refactor: nav-bar create data file

--- space_tourism_website/src/Navbar.js
+++ space_tourism_website/src/Navbar.js
@@ -1,30 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { navData } from './nav-data'
 
 function Navbar() {
   return (
     <nav>
       <ul>
-        <li>
-          <Link>
-            <span>00</span> Home
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <span>01</span> Destination
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <span>02</span> Crew
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <span>03</span> Technology
-          </Link>
-        </li>
+        {navData.map(item => {
+          return (
+            <li key={item.id}>
+              <Link className='nav-link'>
+                <span>{item.id}</span> {item.name}
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
---
+++ space_tourism_website/src/nav-data.js
@@ -0,0 +1,4 @@
+export const navData = [{ id: '00', name: 'Home' },
+{ id: '01', name: 'Destination' },
+{ id: '02', name: 'Crew' },
+{ id: '03', name: 'Technology' }]
