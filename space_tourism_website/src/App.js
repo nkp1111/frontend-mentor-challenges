e4ab32137ea5483e164f6ebc203bb09c914024ae
neeraj <neerajp4321@gmail.com>
2022-10-01 17:53:45 +0530

    Feat: add header and style

--- space_tourism_website/src/App.js
+++ space_tourism_website/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Home from './Home'
-import Navbar from './Navbar'
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Header />
       <Home />
     </div>
   );
---
+++ space_tourism_website/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Header() {
+  return (
+    <header>
+      <img src='https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/8cc344ae40cf01bbc2b23b88a8036ac6e56ae46d/space_tourism_website/src/assets/shared/logo.svg'
+        alt='logo'></img>
+      <div></div>
+      <Navbar />
+    </header>
+  )
+}
+
+export default Header
