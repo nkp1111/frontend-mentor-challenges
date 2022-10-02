31584969108fa152da93c31c3c3f834abac694a9
neeraj <neerajp4321@gmail.com>
2022-10-02 10:08:19 +0530

    Fix: destination navbar position

--- space_tourism_website/src/Destination.js
+++ space_tourism_website/src/Destination.js
@@ -18,28 +18,32 @@ function Destination() {
         <img src='https://github.com/nkp1111/frontend-mentor-challenges/blob/main/space_tourism_website/src/assets/destination/image-moon.png?raw=true' alt='moon_image'></img>
       </div>
       <div>
-        <nav>
-          <ul>
-            {data.destinations.map(item => {
-              return <li key={item.name}>
-                {item.name}
-              </li>
-            })}
-          </ul>
-        </nav>
-        {particularDes.map(item => {
-          return (<article key={item.name}>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <div className='underline'></div>
-            <div className='des-info'>
-              <div>Avg. distance</div>
-              <div>Est. travel time</div>
-              <div>{item.distance}</div>
-              <div>{item.travel}</div>
-            </div>
-          </article>)
-        })}
+        <div className='des-nav-holder'>
+          <nav>
+            <ul>
+              {data.destinations.map(item => {
+                return <li key={item.name}>
+                  {item.name}
+                </li>
+              })}
+            </ul>
+          </nav>
+        </div>
+        <div>
+          {particularDes.map(item => {
+            return (<article key={item.name}>
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+              <div className='underline'></div>
+              <div className='des-info'>
+                <div>Avg. distance</div>
+                <div>Est. travel time</div>
+                <div>{item.distance}</div>
+                <div>{item.travel}</div>
+              </div>
+            </article>)
+          })}
+        </div>
       </div>
     </section>
   )
