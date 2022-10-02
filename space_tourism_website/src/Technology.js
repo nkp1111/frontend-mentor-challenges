c2257dd285104129dcbe0d3681e76c4e5603dcce
neeraj <neerajp4321@gmail.com>
2022-10-02 15:36:13 +0530

    Feat: add technology page initial content

--- space_tourism_website/src/Technology.js
+++ space_tourism_website/src/Technology.js
@@ -15,17 +15,20 @@ function Technology() {
   console.log(technologyData, technology, particularTech);
   return (
     <section className='technology'>
-      {/* <h2><span>03</span> Space launch 101</h2>
+      <h2><span>03</span> Space launch 101</h2>
       <div>
         {particularTech.map(item => {
-          return (<img src={`${item.images.http}`} alt='moon_image'></img>)
+          return (<img
+            key={item.name}
+            src={`${item.images.landscape}`}
+            alt='moon_image'></img>)
         })}
       </div>
       <div className='tech-info'>
         <div className='tech-nav-holder'>
           <nav>
             <ul>
-              {data.technologys.map(item => {
+              {technologyData.map(item => {
                 return <li key={item.name}
                   onClick={() => handleTechnology(item.name)}>
                   {item.name}
@@ -37,9 +40,9 @@ function Technology() {
         <div>
           {particularTech.map(item => {
             return (<article key={item.name}>
-              <h3>{item.name}</h3>
+              <h3>The terminology...</h3>
+              <h4>{item.name}</h4>
               <p>{item.description}</p>
-              <div className='underline'></div>
               <div>
                 <div>Avg. distance</div>
                 <div>Est. travel time</div>
@@ -49,7 +52,7 @@ function Technology() {
             </article>)
           })}
         </div>
-      </div> */}
+      </div>
     </section>
   )
 }
