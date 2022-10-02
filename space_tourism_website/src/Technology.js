732981e9bcf2a710900f2bc897f02491b1ce45df
neeraj <neerajp4321@gmail.com>
2022-10-02 15:19:08 +0530

    Feat: add initial technology file content

--- space_tourism_website/src/Technology.js
+++ space_tourism_website/src/Technology.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import data from './assets/data.json'
+import useGlobalContext from './context'
 
 function Technology() {
+
+  const { technology, handleTechnology } = useGlobalContext()
+  let technologyData = data.technology
+  let newTechnology = technology === undefined ? 'Moon' : technology
+
+  /* To set a particular technology */
+  let particularTech = technologyData.filter(item => {
+    return item.name === newTechnology ? item : null
+  })
+  // console.log(technologyData, technology, particularTech);
   return (
-    <section>
-      This is Technology page
+    <section className='technology'>
+      {/* <h2><span>03</span> Space launch 101</h2>
+      <div>
+        {particularTech.map(item => {
+          return (<img src={`${item.images.http}`} alt='moon_image'></img>)
+        })}
+      </div>
+      <div className='tech-info'>
+        <div className='tech-nav-holder'>
+          <nav>
+            <ul>
+              {data.technologys.map(item => {
+                return <li key={item.name}
+                  onClick={() => handletechnology(item.name)}>
+                  {item.name}
+                </li>
+              })}
+            </ul>
+          </nav>
+        </div>
+        <div>
+          {particularTech.map(item => {
+            return (<article key={item.name}>
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+              <div className='underline'></div>
+              <div>
+                <div>Avg. distance</div>
+                <div>Est. travel time</div>
+                <div>{item.distance}</div>
+                <div>{item.travel}</div>
+              </div>
+            </article>)
+          })}
+        </div>
+      </div> */}
     </section>
   )
 }
