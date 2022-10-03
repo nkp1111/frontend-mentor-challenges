da667ab4f32d5c4005368a1b8ca5f5f08914781c
neeraj <neerajp4321@gmail.com>
2022-10-03 15:55:36 +0530

    Fix: remove sidebar file

--- space_tourism_website/src/Header/Navbar.js
+++ space_tourism_website/src/Header/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { navData } from './nav-data'
 import useGlobalContext from '../context'
 import { FaBars } from 'react-icons/fa'
@@ -10,11 +9,14 @@ function Navbar() {
   const { navbar, sidebar, handleSidebar } = useGlobalContext()
   console.log(sidebar);
   return (
-    <nav className={navbar ? "" : "menu"}>
-      <ul>
-        {navbar ?
-          <NavItem navData={navData} />
-          : <li onClick={() => handleSidebar(sidebar)}><FaBars /></li>}
+    <nav className={navbar ? '' : 'menu'}>
+      <ul className={sidebar ? 'sidebar' : ''}>
+        {navbar
+          ? <NavItem navData={navData} />
+          : sidebar
+            ? <NavItem navData={navData} />
+            : <li onClick={() => handleSidebar(sidebar)}><FaBars /></li>
+        }
       </ul>
     </nav>
   )
