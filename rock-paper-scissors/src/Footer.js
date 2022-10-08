d1f121977315135e0a157bcabfa33cf019a780b7
neeraj <neerajp4321@gmail.com>
2022-10-08 12:30:39 +0530

    Refactor: start again

---
+++ rock-paper-scissors/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>.
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  )
+}
+
+export default Footer
