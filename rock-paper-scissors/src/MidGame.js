dc18290fb1184762e656048cc38a25c7876d9088
neeraj <neerajp4321@gmail.com>
2022-10-08 22:12:15 +0530

    Refactor: grid arrangement of midgame

--- rock-paper-scissors/src/MidGame.js
+++ rock-paper-scissors/src/MidGame.js
@@ -7,8 +7,10 @@ function MidGame() {
   return (
     <div className='midgame'>
       <div>You Picked</div>
+      <div></div>
       <div>The House Picked</div>
       <div className={playerChoice.name}><img src={playerChoice.img} alt={playerChoice.name} /></div>
+      <div></div>
       <div className={computerChoice.name}><img src={computerChoice.img} alt={computerChoice.name} /></div>
     </div>
   )
