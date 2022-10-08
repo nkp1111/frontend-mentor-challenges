1b649226d6287d64e1000d539f268c874d3d6ed1
neeraj <neerajp4321@gmail.com>
2022-10-08 14:16:12 +0530

    Feat: add final style to modal

--- rock-paper-scissors/src/Modal.js
+++ rock-paper-scissors/src/Modal.js
@@ -4,7 +4,7 @@ function Modal() {
   return (
     <div id="modal">
       <div>
-        <div>Rules</div>
+        <p>Rules</p>
         <div><img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/2b55ec5c74cce415cd1257c4757ad2b016616421/rock-paper-scissors/src/assets/images/icon-close.svg" alt="close button" /></div>
       </div>
       <div>
