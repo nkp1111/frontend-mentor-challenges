b9cd4d54eb3da6a5322491763108003cdf64c86e
neeraj <neerajp4321@gmail.com>
2022-10-08 15:13:29 +0530

    Refactor: add classname

--- rock-paper-scissors/src/Game.js
+++ rock-paper-scissors/src/Game.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Game() {
   return (
     <div id="game">
-      <div>
+      <div className='score-board'>
         <img
           src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/logo.svg"
           alt="logo" />
-        <div className='score-board'>
+        <div className='score'>
           <p>Score</p>
           <p>10</p>
         </div>
