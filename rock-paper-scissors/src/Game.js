c515cd17d8e25505711b78db60f78b52a4c0fbb3
neeraj <neerajp4321@gmail.com>
2022-10-08 17:07:12 +0530

    Refactor: add var for game img and background

--- rock-paper-scissors/src/Game.js
+++ rock-paper-scissors/src/Game.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GameChoice from './gameChoice'
 
 function Game() {
   return (
@@ -12,16 +13,7 @@ function Game() {
           <p>10</p>
         </div>
       </div>
-      <div className='game-choice'>
-        <img
-          src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/bg-triangle.svg"
-          alt="triangle" />
-        <div>
-          <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-paper.svg" alt="paper sign logo" />
-        </div>
-        <div>      <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-scissors.svg" alt="scissors sign logo" /></div>
-        <div>       <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-rock.svg" alt="rock sign logo" /></div>
-      </div>
+      <GameChoice />
     </div>
   )
 }
---
+++ rock-paper-scissors/src/gameChoice.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function GameChoice() {
+  return (
+    <div className='game-choice'>
+      <img
+        src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/bg-triangle.svg"
+        alt="triangle" />
+      <div>
+        <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-paper.svg" alt="paper sign logo" />
+      </div>
+      <div>      <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-scissors.svg" alt="scissors sign logo" /></div>
+      <div>       <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-rock.svg" alt="rock sign logo" /></div>
+    </div>
+  )
+}
+
+export default GameChoice
