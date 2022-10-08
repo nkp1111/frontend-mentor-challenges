efc9df51975512b69acbe9f75e9b49d052dcc3c4
neeraj <neerajp4321@gmail.com>
2022-10-08 14:40:51 +0530

    Feat: add game file

---
+++ rock-paper-scissors/src/Game.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Game() {
+  return (
+    <div id="game">
+      <div>
+        <img
+          src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/logo.svg"
+          alt="logo" />
+        <div>
+          <p>Score</p>
+          <p>0</p>
+        </div>
+      </div>
+      <div>
+
+        <img
+          src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/bg-triangle.svg"
+          alt="triangle" />
+        <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-paper.svg" alt="" />
+        <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-scissors.svg" alt="" />
+        <img src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-rock.svg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Game
