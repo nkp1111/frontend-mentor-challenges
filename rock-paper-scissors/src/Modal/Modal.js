9f9904d9588bcd3feb42269998cff41ccfacdf42
neeraj <neerajp4321@gmail.com>
2022-10-09 18:13:57 +0530

    Feat: add image change to modal with game mode

--- rock-paper-scissors/src/Modal/Modal.js
+++ rock-paper-scissors/src/Modal/Modal.js
@@ -18,7 +18,9 @@ function Modal() {
       </div>
       <div>
         <img
-          src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/2b55ec5c74cce415cd1257c4757ad2b016616421/rock-paper-scissors/src/assets/images/image-rules.svg"
+          src={gameMode === 'easy'
+            ? 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/2b55ec5c74cce415cd1257c4757ad2b016616421/rock-paper-scissors/src/assets/images/image-rules.svg'
+            : 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b432a35d7eca4159e5628892ade55edd941df210/rock-paper-scissors/src/assets/images/image-rules-bonus.svg'}
           alt="rock paper scissors rules" />
       </div>
       <button className='change-mode'
