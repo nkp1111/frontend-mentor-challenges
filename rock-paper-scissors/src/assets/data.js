ce75f123ec3ec317f3582616ed9867301981faf1
neeraj <neerajp4321@gmail.com>
2022-10-09 18:26:47 +0530

    Feat: modify data to add spock and lizard

--- rock-paper-scissors/src/assets/data.js
+++ rock-paper-scissors/src/assets/data.js
@@ -1,7 +1,9 @@
 const data = [
   { img: 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-paper.svg', name: 'paper' },
   { img: 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-scissors.svg', name: 'scissors' },
-  { img: 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-rock.svg', name: 'rock' }
+  { img: 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/icon-rock.svg', name: 'rock' },
+  { img: 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/786bb0bb36e637e461be65f5f87fc1647b4cb395/rock-paper-scissors/src/assets/images/icon-lizard.svg', name: 'lizard' },
+  { img: 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/786bb0bb36e637e461be65f5f87fc1647b4cb395/rock-paper-scissors/src/assets/images/icon-spock.svg', name: 'spock' }
 ]
 
 export default data
