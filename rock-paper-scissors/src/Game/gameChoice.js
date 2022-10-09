106d595f45f97477a1c5fed8734439e483c9e7d7
neeraj <neerajp4321@gmail.com>
2022-10-09 18:34:13 +0530

    Feat: modify data for diff img and styles

--- rock-paper-scissors/src/Game/gameChoice.js
+++ rock-paper-scissors/src/Game/gameChoice.js
@@ -5,6 +5,8 @@ import useGlobalContext from '../context'
 function GameChoice() {
 
   const { setPlayerChoice, gameMode } = useGlobalContext()
+
+  let gameData = gameMode === 'easy' ? data.slice(0, 3) : data
   return (
     <section className='startgame'>
       <h2>Pick A Choice</h2>
@@ -13,7 +15,7 @@ function GameChoice() {
           ? 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/bg-triangle.svg'
           : 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b432a35d7eca4159e5628892ade55edd941df210/rock-paper-scissors/src/assets/images/bg-pentagon.svg'}
         alt="triangle" />
-      {data.map(item => {
+      {gameData.map(item => {
         return (
           <div key={item.name}
             className={item.name}
