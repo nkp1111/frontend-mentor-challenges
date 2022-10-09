4dfaf64f014511a23c27d64408181b1ad4a2e40a
neeraj <neerajp4321@gmail.com>
2022-10-09 17:25:21 +0530

    Refactor : scoreboard div

--- rock-paper-scissors/src/Game/Scoreboard.js
+++ rock-paper-scissors/src/Game/Scoreboard.js
@@ -5,7 +5,8 @@ function GameScore() {
 
   const { score } = useGlobalContext()
   return (
-    <div className='score-board'>
+    <section className='score-board'>
+      <h2>Score Board</h2>
       <img
         src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/logo.svg"
         alt="logo" />
@@ -13,7 +14,7 @@ function GameScore() {
         <p>Score</p>
         <p>{score}</p>
       </div>
-    </div>
+    </section>
   )
 }
 
