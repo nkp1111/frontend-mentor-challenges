786bb0bb36e637e461be65f5f87fc1647b4cb395
neeraj <neerajp4321@gmail.com>
2022-10-09 18:21:21 +0530

    Feat: add startgame triangle to pentagram change

--- rock-paper-scissors/src/Game/gameChoice.js
+++ rock-paper-scissors/src/Game/gameChoice.js
@@ -4,12 +4,14 @@ import useGlobalContext from '../context'
 
 function GameChoice() {
 
-  const { setPlayerChoice } = useGlobalContext()
+  const { setPlayerChoice, gameMode } = useGlobalContext()
   return (
     <section className='startgame'>
       <h2>Pick A Choice</h2>
       <img
-        src="https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/bg-triangle.svg"
+        src={gameMode === 'easy'
+          ? 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/98c203fa86b9f72c246559b69755eb87e8d41bb6/rock-paper-scissors/src/assets/images/bg-triangle.svg'
+          : 'https://raw.githubusercontent.com/nkp1111/frontend-mentor-challenges/b432a35d7eca4159e5628892ade55edd941df210/rock-paper-scissors/src/assets/images/bg-pentagon.svg'}
         alt="triangle" />
       {data.map(item => {
         return (
