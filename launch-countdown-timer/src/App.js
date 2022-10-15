b5925690bb33a82df20c6b4bc98645b2557db851
neeraj <neerajp4321@gmail.com>
2022-10-15 15:23:53 +0530

    Refactor: countdown timer add heading

--- launch-countdown-timer/src/App.js
+++ launch-countdown-timer/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-      <h1>We're launching soon</h1>
-      <div>
-        <div>08</div>
-        <div>23</div>
-        <div>55</div>
-        <div>41</div>
-        <div>Days</div>
-        <div>Hours</div>
-        <div>Minutes</div>
-        <div>Seconds</div>
-      </div>
-      <div className='mountain-bed'></div>
-    </div>
+    <>
+      <Header />
+      <section>
+        <h2>We're launching soon</h2>
+        <div className='timer'>
+          <div>08</div>
+          <div>23</div>
+          <div>55</div>
+          <div>41</div>
+          <div>Days</div>
+          <div>Hours</div>
+          <div>Minutes</div>
+          <div>Seconds</div>
+        </div>
+        <div className='mountain-bed'></div>
+      </section>
+    </>
   );
 }
 
---
+++ launch-countdown-timer/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h1>
+      Launch countdown timer
+    </h1>
+  )
+}
+
+export default Header
