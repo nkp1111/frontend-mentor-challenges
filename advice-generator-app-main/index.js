e274d10cc2febd2befe7d6fd2c617caf6594902a
neeraj <neerajp4321@gmail.com>
2022-10-16 11:58:09 +0530

    Feat: add random quote at load and click

--- advice-generator-app-main/index.js
+++ advice-generator-app-main/index.js
@@ -1,8 +1,25 @@
+const article = document.getElementById('advice')
+const dice = document.getElementById('dice')
+const adviceNum = document.querySelector('.advice-number')
 
 fetch("https://type.fit/api/quotes")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    getAdvice(data);
   });
+
+const setAdvice = (data) => {
+  const randomNum = Math.floor(Math.random() * 500)
+  const randomAdvice = data[randomNum]
+  article.innerText = `"${randomAdvice.text}"`
+  adviceNum.innerText = ` #${randomNum + 1}`
+}
+
+const getAdvice = (data) => {
+  setAdvice(data)
+  dice.addEventListener('click', () => {
+    setAdvice(data)
+  })
+}
