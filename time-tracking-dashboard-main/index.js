41a2ac86583573fb8bb2039cce2385c6613c59a2
neeraj <neerajp4321@gmail.com>
2022-10-23 12:00:24 +0530

    Feat: complete project time-tracking dashboard

--- time-tracking-dashboard-main/index.js
+++ time-tracking-dashboard-main/index.js
@@ -1,18 +1,53 @@
 const links = document.querySelectorAll('li')
 const timeframe = document.querySelectorAll('.time-frame')
-let currentTimeframe = 'Daily'
+const currentTimes = document.querySelectorAll('.current-time')
+const prevTimes = document.querySelectorAll('.prev-time')
+let currentTimeframe = 'daily'
+const dataMap = new Map()
 
 for (let link of links) {
-  link.style.color = 'hsl(235, 45%, 61%)'
   link.addEventListener('click', (e) => {
-    currentTimeframe = e.target.textContent
+    currentTimeframe = e.target.textContent.toLowerCase()
     e.target.style.color = 'white'
-    timeframe.forEach(d => {
-      d.innerText = currentTimeframe
-    })
+    changeTimeFrameText()
+    changeCurrentAndPrevTime()
   })
 }
 
-timeframe.forEach(d => {
-  d.innerText = currentTimeframe
-})
+const changeTimeFrameText = () => {
+  timeframe.forEach(d => {
+    if (currentTimeframe === 'daily') {
+      d.innerText = 'Yesterday'
+    } else if (currentTimeframe === 'weekly') {
+      d.innerText = 'Last Week'
+    } else if (currentTimeframe === 'monthly') {
+      d.innerText = 'Last Month'
+    }
+  })
+}
+
+const changeCurrentAndPrevTime = () => {
+  fetch('./data.json')
+    .then(res => res.json())
+    .then(data => showData(data))
+    .catch(err => console.log(err))
+}
+
+const showData = (data) => {
+  data.map(d => {
+    dataMap.set(d.title.toLowerCase(), d.timeframes)
+  })
+
+  currentTimes.forEach(time => {
+    const times = dataMap.get(time.classList[1])
+    time.innerText = times[currentTimeframe]['current'] + 'hrs'
+  })
+
+  prevTimes.forEach(time => {
+    const times = dataMap.get(time.classList[1])
+    time.innerText = ' - ' + times[currentTimeframe]['current'] + 'hrs'
+  })
+}
+
+changeTimeFrameText()
+changeCurrentAndPrevTime()
