a2e433e1d52c3d7b6a8f1e638dd633d0e79a3cde
neeraj <neerajp4321@gmail.com>
2022-10-29 10:01:20 +0530

    Feat: add initial html content

--- ip-address-tracker/src/App.js
+++ ip-address-tracker/src/App.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import Footer from './footer'
+import { FiChevronRight } from 'react-icons/fi'
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <main className="App">
+        <h1>IP Address Tracker</h1>
 
-    </div>
+        <form>
+          <input type='text' placeholder='Search for any IP address or domain' name='location' id='location' />
+          <button><FiChevronRight /></button>
+        </form>
+
+        <article>
+          <h3>IP Address</h3>
+          <p></p>
+        </article>
+
+        <article>
+          <h3>Location</h3>
+          <p></p>
+        </article>
+
+        <article>
+          <h3> Timezone</h3>
+          <p>UTC</p>
+        </article>
+
+        <article>
+          <h3>ISP</h3>
+          <p></p>
+        </article>
+
+      </main>
+      <Footer />
+    </>
   );
 }
 
