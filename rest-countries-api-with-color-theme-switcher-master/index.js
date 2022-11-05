b9ea9df27578190eb1ffe084bb3abadc74d35a2d
neeraj <neerajp4321@gmail.com>
2022-11-05 14:04:03 +0530

    Feat: add initial country inside page

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -9,18 +9,20 @@ app.use(express.static(__dirname + '/public'))
 const url = 'https://restcountries.com/v3.1/name/united'
 
 axios.get(url)
-  .then(d => setCountry(d))
+  .then(d => setCountrys(d))
   .catch(err => console.log(err))
 
-let country
-const setCountry = (data) => {
-  country = data.data.slice(0, 8)
-  // country = data.slice(0, 8)
+let countrys
+const setCountrys = (data) => {
+  countrys = data.data.slice(0, 8)
+  // countrys = data.slice(0, 8)
 }
 
 app.get('/', (req, res) => {
-  console.log(country);
-  res.render('index', { country })
+  console.log(countrys && countrys[0].name.official, countrys[0].population, countrys[0].region, countrys[0].flags.svg);
+  // detail
+  console.log(countrys[0].name.nativeName, countrys[0].subregion, countrys[0].capital)
+  res.render('index', { countrys })
 })
 
 app.listen(3000, () => {
