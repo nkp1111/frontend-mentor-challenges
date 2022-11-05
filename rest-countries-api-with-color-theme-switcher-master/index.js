e33b9bd66cf595392200fee3834c81151dd868b7
neeraj <neerajp4321@gmail.com>
2022-11-05 12:11:17 +0530

    Feat: add css file and initial styles

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 
 const url = 'https://restcountries.com/v3.1/all'
 
