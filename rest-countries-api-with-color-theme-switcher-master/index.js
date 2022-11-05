a8011efd5a8f19aa71842042a1e7f8e6df8084a3
neeraj <neerajp4321@gmail.com>
2022-11-05 13:35:20 +0530

    Feat: add styles to header

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -6,18 +6,21 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 
-const url = 'https://restcountries.com/v3.1/all'
+const url = 'https://restcountries.com/v3.1/name/united'
 
 axios.get(url)
   .then(d => setCountry(d))
   .catch(err => console.log(err))
 
+let country
 const setCountry = (data) => {
-  // console.log(data)
+  country = data.data.slice(0, 8)
+  // country = data.slice(0, 8)
 }
 
 app.get('/', (req, res) => {
-  res.render('index')
+  console.log(country);
+  res.render('index', { country })
 })
 
 app.listen(3000, () => {
