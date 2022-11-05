66d31b0ed26a52e6e53f37aa23d5cb979295350f
neeraj <neerajp4321@gmail.com>
2022-11-05 11:47:56 +0530

    Feat: add country data url and axios

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
+const url = 'https://restcountries.com/v3.1/all'
+
+axios.get(url)
+  .then(d => setCountry(d))
+  .catch(err => console.log(err))
+
+const setCountry = (data) => {
+  // console.log(data)
+}
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(3000, () => {
   console.log('Active on port 3000')
