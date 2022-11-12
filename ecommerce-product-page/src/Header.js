147d22a8e924bf055f75ced7a1fa46ba30e41d3f
neeraj <neerajp4321@gmail.com>
2022-11-12 21:23:44 +0530

    Feat: add header component

---
+++ ecommerce-product-page/src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from './assets/images/logo.svg'
+import cart from './assets/images/icon-cart.svg'
+import avatar from './assets/images/image-avatar.png'
+import { data } from './assets/navData'
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+      <nav>
+        <div>
+          <ul>
+            {data.map(d => {
+              return (<li key={d}>{d}</li>)
+            })}
+          </ul>
+        </div>
+      </nav>
+      <div id="info">
+        <img src={cart} alt="cart" />
+        <img src={avatar} alt="avatar" />
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ ecommerce-product-page/src/assets/navData.js
@@ -0,0 +1,8 @@
+export const data = [
+  "Collections",
+  "Men",
+  "Women",
+  "About",
+  "Contact"
+]
+
