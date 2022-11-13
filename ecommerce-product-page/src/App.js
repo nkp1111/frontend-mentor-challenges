c5930c4cb6ad7bdef635298d38a4db5004194d8b
neeraj <neerajp4321@gmail.com>
2022-11-13 10:16:41 +0530

    Feat: add main html content

--- ecommerce-product-page/src/App.js
+++ ecommerce-product-page/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './header';
 import Footer from './footer'
-
+import Main from './main'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
