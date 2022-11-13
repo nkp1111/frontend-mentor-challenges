26fe620348e626d0f1cb95857f3405f8a84ef083
neeraj <neerajp4321@gmail.com>
2022-11-13 15:55:30 +0530

    Feat: refactor thumbnails and add styles for hover

--- ecommerce-product-page/src/main/ImageSection.js
+++ ecommerce-product-page/src/main/ImageSection.js
@@ -8,18 +8,25 @@ import ImageThumb2 from '../assets/images/image-product-2-thumbnail.jpg'
 import ImageThumb3 from '../assets/images/image-product-3-thumbnail.jpg'
 import ImageThumb4 from '../assets/images/image-product-4-thumbnail.jpg'
 
+const images = [Image1, Image2, Image3, Image4]
+const thumbnails = [ImageThumb1, ImageThumb2, ImageThumb3, ImageThumb4]
+
 function ImageSection() {
   return (
     <section className='m-3 px-3'>
       <h2 className='opacity-0 d-none'>Product image</h2>
       <div className='mb-4' id="main-img-holder">
+        { }
         <img src={Image1} alt="product full view" className='w-100 rounded-4' />
       </div>
       <div id='thumbnail-holder' className='d-flex w-100 justify-content-between'>
-        <div><img src={ImageThumb1} alt="product preview" /></div>
-        <div><img src={ImageThumb2} alt="product preview" /></div>
-        <div><img src={ImageThumb3} alt="product preview" /></div>
-        <div><img src={ImageThumb4} alt="product preview" /></div>
+        {thumbnails.map((thumbnail, ind) => {
+          return (
+            <div key={ind}>
+              <img src={thumbnail} alt="product preview" />
+            </div>
+          )
+        })}
       </div>
     </section>
   )
