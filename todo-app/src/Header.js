5910dfe7f99b276a48b8fdb6bb818bd536829a21
neeraj <neerajp4321@gmail.com>
2022-11-27 10:57:48 +0530

    Feat: complete style header

--- todo-app/src/Header.js
+++ todo-app/src/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { FiSun, FiCheck } from 'react-icons/fi'
+import { FiCheck } from 'react-icons/fi'
 import { FaMoon } from 'react-icons/fa'
 import { GrClose } from 'react-icons/gr'
+import { RiSunFill } from 'react-icons/ri'
+
+
 
 
 function Header() {
   return (
     <header>
       <h1>Todo</h1>
-      <FiSun />
+      <RiSunFill class="mode-change" />
       {/* <FaMoon /> */}
     </header>
   )
