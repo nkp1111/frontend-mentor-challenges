8bce5c8ffaa4f92c19cf39f1125f037a041fa688
neeraj <neerajp4321@gmail.com>
2022-12-03 09:43:28 +0530

    Feat: add sidebar html

---
+++ multi-step-form/src/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import SidebarData from './assets/data/sidebarData'
+
+function Sidebar() {
+  return (
+    <div id="sidebar">
+      <ul>
+        {SidebarData.map(item => {
+          return (
+            <li key={item.id}>
+              <span>{item.id}</span>
+              <span>
+                <p>Step {item.id}</p>
+                <p>{item.type}</p>
+              </span>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ multi-step-form/src/assets/data/sidebarData.js
@@ -0,0 +1,9 @@
+const sidebarData = [
+  { id: 1, type: "your info" },
+  { id: 2, type: "select plan" },
+  { id: 3, type: "add-ons" },
+  { id: 4, type: "summary" },
+]
+
+
+export default sidebarData
