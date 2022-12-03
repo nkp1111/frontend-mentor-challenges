0726c8daae3feecb11d569681ae331aea77f7b64
neeraj <neerajp4321@gmail.com>
2022-12-03 12:35:43 +0530

    Feat: add footer and separate heading and footer

--- multi-step-form/src/App.js
+++ multi-step-form/src/App.js
@@ -6,23 +6,28 @@ import PickAddOns from './pages/PickAddOns'
 import Summary from './pages/Summary'
 import ThankYou from './pages/ThankYou'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Footer from './Footer'
 
 function App() {
   return (
-    <main className="App">
-      <h1>Multi Step Form</h1>
-      <Router>
-        <Sidebar />
-        <Routes>
-          <Route exact path="/" element={<PersonalInfo />}></Route>
-          <Route path="/personalInfo" element={<PersonalInfo />} ></Route>
-          <Route path="/planSelect" element={<SelectPlan />}></Route>
-          <Route path="/pickAddOns" element={<PickAddOns />} ></Route>
-          <Route path='/summary' element={<Summary />}></Route>
-          <Route path='/thankYou' element={<ThankYou />}></Route>
-        </Routes>
-      </Router>
-    </main>
+    <>
+      <main className="App">
+        <h1>Multi Step Form</h1>
+        <Router>
+          <Sidebar />
+          <Routes>
+            <Route exact path="/" element={<PersonalInfo />}></Route>
+            <Route path="/personalInfo" element={<PersonalInfo />} ></Route>
+            <Route path="/planSelect" element={<SelectPlan />}></Route>
+            <Route path="/pickAddOns" element={<PickAddOns />} ></Route>
+            <Route path='/summary' element={<Summary />}></Route>
+            <Route path='/thankYou' element={<ThankYou />}></Route>
+          </Routes>
+          <Footer />
+        </Router>
+      </main>
+    </>
+
   );
 }
 
---
+++ multi-step-form/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/nkp1111">Neeraj Parmar</a>.
+    </footer>
+  )
+}
+
+export default Footer
