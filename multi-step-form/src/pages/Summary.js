f14d34d619c158bcf094ac6c50f9ab1bb62cae91
neeraj <neerajp4321@gmail.com>
2022-12-03 20:31:18 +0530

    Feat: add style to summary page

--- multi-step-form/src/pages/Summary.js
+++ multi-step-form/src/pages/Summary.js
@@ -10,29 +10,31 @@ function Summary() {
       <form>
         <ul>
           {/*title */}
-          <li>
-            <span>
-              <p>Arcade(Monthly)</p>
-              <a>change</a>
-            </span>
-            <span>$9/mon</span>
-          </li>
-
-          <li>
-            <span>Service</span>
-            <span>$5/mon</span>
-          </li>
-          <li>
-            <span>Service</span>
-            <span>$5/mon</span>
-          </li>
+          <div>
+            <li>
+              <span>
+                <p>Arcade(Monthly)</p>
+                <a>change</a>
+              </span>
+              <span>$9/mon</span>
+            </li>
+            <hr />
+            <li>
+              <span>Service</span>
+              <span>+$5/mon</span>
+            </li>
+            <li>
+              <span>Service</span>
+              <span>+$5/mon</span>
+            </li>
+          </div>
 
           {/*total */}
-          <li>
+          <li className='total-amount'>
             <span>
-              <p>Total permonth</p>
+              <p>Total (permonth)</p>
             </span>
-            <span>$9/mon</span>
+            <span>+$9/mon</span>
           </li>
         </ul>
         <div className='btn-holder'>
