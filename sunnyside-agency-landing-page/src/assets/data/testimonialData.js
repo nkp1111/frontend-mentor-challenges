261a06ed33f201c1c3bbd7a755ad77b4ac345c54
neeraj <neerajp4321@gmail.com>
2022-12-10 18:39:03 +0530

    Feat: add testimonial section style

--- sunnyside-agency-landing-page/src/assets/data/testimonialData.js
+++ sunnyside-agency-landing-page/src/assets/data/testimonialData.js
@@ -3,19 +3,21 @@ const testimonialData = [
     id: 1,
     name: "Emily R.",
     position: "Marketing Director",
-    comment: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit."
+    comment: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    profileImage: "./assets/images/image-emily.jpg",
   },
   {
     id: 2,
     name: "Thomas S.",
     position: "Chief Operating Officer",
-    comment: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience."
+    comment: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+    profileImage: "../assets/images/image-thomas.jpg",
   },
   {
     id: 3,
     name: " Jennie F.",
     position: "Business Owner",
-    comment: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!"
+    comment: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
   },
 ]
 
--- sunnyside-agency-landing-page/src/main/TestimonialSection.js
+++ sunnyside-agency-landing-page/src/main/TestimonialSection.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import { testimonialData } from '../assets/data/testimonialData'
+import EmillyImage from '../assets/images/image-emily.jpg'
+import ThomasImage from '../assets/images/image-thomas.jpg'
+import JennieImage from '../assets/images/image-jennie.jpg'
+let avatarImages = [EmillyImage, ThomasImage, JennieImage]
 
 function TestimonialSection() {
   return (
-    <section>
-      <h2>Client testimonials</h2>
-      {testimonialData.map(item => {
-        return (
-          <article>
-            <div>
-              <h3>{item.name}</h3>
-              <p>{item.position}</p>
-            </div>
-            <div>
-              <img></img>
-              <p>{item.comment}</p>
-            </div>
-          </article>
-        )
-      })}
+    <section id="testimonial-section">
+      <h2 className='testimonial-heading'>Client testimonials</h2>
+      <div className='row gy-3'>
+        {testimonialData.map(item => {
+          return (
+            <article className='col-4 user d-flex flex-column'>
+              <div className='order-1'>
+                <h3 className='user-name'>{item.name}</h3>
+                <p className='user-position'>{item.position}</p>
+              </div>
+              <div className='order-0'>
+                <img src={avatarImages[item.id - 1]} alt="profile avatar" className='user-image' />
+                <p className='user-text'>{item.comment}</p>
+              </div>
+            </article>
+          )
+        })}
+      </div>
     </section>
   )
 }
