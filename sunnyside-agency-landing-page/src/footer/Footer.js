16ca2f84b6c6c2e40678bad0744224f1d24d7015
neeraj <neerajp4321@gmail.com>
2022-12-10 19:20:16 +0530

    Feat: add style to footer

--- sunnyside-agency-landing-page/src/footer/Footer.js
+++ sunnyside-agency-landing-page/src/footer/Footer.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import Nav from '../header/Nav'
+import facebook from '../assets/images/icon-facebook.svg'
+import instagram from '../assets/images/icon-instagram.svg'
+import twitter from '../assets/images/icon-twitter.svg'
+import pinterest from '../assets/images/icon-pinterest.svg'
 
 function Footer() {
   return (
-    <footer>
-      <p>sunnyside</p>
-      <Nav />
-      <ul>
-        <li>facebook</li>
-        <li>facebook</li>
-        <li>facebook</li>
-        <li>facebook</li>
+    <footer
+      className='d-flex 
+    flex-column 
+    align-items-center'>
+      <p className='footer-title'>sunnyside</p>
+      <ul className='footer-links'>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+      </ul>
+      <ul className='footer-icons'>
+        <li>
+          <img src={facebook} alt='facebook logo' />
+        </li>
+        <li>
+          <img src={instagram} alt='instagram logo' />
+        </li>
+        <li>
+          <img src={twitter} alt='twitter logo' />
+        </li>
+        <li>
+          <img src={pinterest} alt='pinterest logo' />
+        </li>
       </ul>
     </footer>
   )
--- sunnyside-agency-landing-page/src/main/ImageSection.js
+++ sunnyside-agency-landing-page/src/main/ImageSection.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import milkbottlesImageDesktop from '../assets/images/desktop/image-gallery-milkbottles.jpg'
+import milkbottlesImageMobile from '../assets/images/mobile/image-gallery-milkbottles.jpg'
 import orangeImageDesktop from '../assets/images/desktop/image-gallery-orange.jpg'
 import orangeImageMobile from '../assets/images/mobile/image-gallery-orange.jpg'
 import coneImageDesktop from '../assets/images/desktop/image-gallery-cone.jpg'
@@ -8,22 +10,28 @@ import sugarcubesImageMobile from '../assets/images/mobile/image-gallery-sugarcu
 
 function ImageSection() {
   return (
-    <section>
-      <h2>Image Section</h2>
-      <div>
-        <picture>
+    <section id="image-section">
+      <h2 className='hidden-heading'>Image Section</h2>
+      <div className='row'>
+        <picture className='col-3'>
+          <source
+            srcSet={milkbottlesImageMobile}
+            media="(max-width: 700px)"></source>
+          <img src={milkbottlesImageDesktop} alt="orange"></img>
+        </picture>
+        <picture className='col-3'>
           <source
             srcSet={orangeImageMobile}
             media="(max-width: 700px)"></source>
           <img src={orangeImageDesktop} alt="orange"></img>
         </picture>
-        <picture>
+        <picture className='col-3'>
           <source
             srcSet={coneImageMobile}
             media="(max-width: 700px)"></source>
           <img src={coneImageDesktop} alt="orange"></img>
         </picture>
-        <picture>
+        <picture className='col-3'>
           <source
             srcSet={sugarcubesImageMobile}
             media="(max-width: 700px)"></source>
