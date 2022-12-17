0f6eb294fc0af634894cb9b50d0e6355736a9e24
neeraj <neerajp4321@gmail.com>
2022-12-17 11:44:22 +0530

    Feat: add data for pages

---
+++ room-homepage/src/assets/data.js
@@ -0,0 +1,38 @@
+const navbarData = [
+  "home",
+  "shop",
+  "about",
+  "contact",
+]
+
+const mainData = [
+  {
+    id: 1,
+    heading: "Discover innovative ways to decorate",
+    description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life.Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    image: {
+      mobile: "",
+      desktop: "",
+    },
+  },
+  {
+    id: 2,
+    heading: "We are available all across the globe",
+    description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country.Find the branch nearest you using our store locator.Any questions? Don't hesitate to contact us today.",
+    image: {
+      mobile: "",
+      desktop: "",
+    },
+  },
+  {
+    id: 3,
+    heading: "Manufactured with the best materials",
+    description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible.With three decades of experience in this industry, we understand what customers want for their home and office.",
+    image: {
+      mobile: "",
+      desktop: "",
+    },
+  },
+]
+
+export { navbarData, mainData }
