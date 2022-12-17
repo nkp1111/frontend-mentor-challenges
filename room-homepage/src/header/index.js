3e3824b56e0582b2e973670b702dc60c3573a7ba
neeraj <neerajp4321@gmail.com>
2022-12-17 12:08:42 +0530

    Feat: add initial header

--- room-homepage/src/header/index.js
+++ room-homepage/src/header/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { navbarData } from '../assets/data'
+import logo from '../assets/images/logo.svg'
+import hamburgerIcon from '../assets/images/icon-hamburger.svg'
+
 
 function Header() {
   return (
     <header>
-      Header
+      <nav className='navbar navbar-expand-sm'>
+        <img src={logo} alt="room logo" className='navbar-brand' />
+        <button className='navbar-toggler' data-bs-toggle="collapse" data-bs-target="#headerNavbar">
+          <img src={hamburgerIcon} alt="hamburger menu" />
+        </button>
+        <div id="headerNavbar" className='collapse navbar-collapse'>
+          <ul className='navbar-nav'>
+            {navbarData.map((item, ind) => {
+              return (
+                <li key={ind}
+                  className="nav-item">
+                  {item}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+      </nav>
     </header>
   )
 }
