0c7fd4cda5d3fccdb1160fa63dd790b2f79e3582
neeraj <neerajp4321@gmail.com>
2022-12-17 11:56:09 +0530

    Feat: add header, footer and initial styles

--- room-homepage/src/App.js
+++ room-homepage/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './header'
+import Footer from './footer'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Footer />
     </div>
   );
 }
