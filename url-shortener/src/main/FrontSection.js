5764e6734d94ee0231db69898d2147e6c05ea53d
neeraj <neerajp4321@gmail.com>
2022-12-18 15:04:53 +0530

    Feat: front section style complete

--- url-shortener/src/main/FrontSection.js
+++ url-shortener/src/main/FrontSection.js
@@ -3,15 +3,15 @@ import frontImage from '../assets/images/illustration-working.svg'
 
 function FrontSection() {
   return (
-    <section>
+    <section className='container-fluid'>
       {/* Front section */}
       <div className='row'>
-        <div className='col-md-6'>
-          <h2>More than just shorter links</h2>
+        <div className='front-section-info col-md-6'>
+          <h2 className='front-section-heading'>More than just shorter links</h2>
           <p>
             Build your brand’s recognition and get detailed insights
             on how your links are performing.</p>
-          <button>Get Started</button>
+          <button className='front-section-btn btn'>Get Started</button>
         </div>
         <div className='col-md-6'>
           <picture>
