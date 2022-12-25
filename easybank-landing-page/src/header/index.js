cce66af30cd1bef5513ff886303297e8dc4f23ce
neeraj <neerajp4321@gmail.com>
2022-12-25 17:35:27 +0530

    Feat: add header styles

--- easybank-landing-page/src/header/index.js
+++ easybank-landing-page/src/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { logo, navData } from '../assets/data'
+import { logo, navData, hamburgerLogo, closeIcon } from '../assets/data'
 
 function index() {
   return (
@@ -7,19 +7,24 @@ function index() {
       <nav className='navbar navbar-expand-md'>
         <img src={logo} alt="logo" className='navbar-brand' />
 
-        <div className='navbar navbar-collapse' id="headerNav">
+        <div className='navbar-toggler' role="button" data-bs-target="#headerNav" data-bs-toggle="collapse">
+          <img src={hamburgerLogo} alt="hamburger menu" />
+        </div>
+
+        <div className='collapse navbar-collapse' id="headerNav">
           <ul className='navbar-nav'>
             {navData.map(item => {
               return (
                 <li key={item}
                   className="nav-item">
                   {item}
+                  <span className='bottom-line'></span>
                 </li>
               )
             })}
           </ul>
 
-          <button className='btn'>
+          <button className='btn invite-btn'>
             Request Invite
           </button>
         </div>
--- easybank-landing-page/src/main/index.js
+++ easybank-landing-page/src/main/index.js
@@ -14,7 +14,7 @@ function index() {
             Take your financial life online. Your Easybank account will be a one-stop-shop
             for spending, saving, budgeting, investing, and much more
           </p>
-          <button className='btn'>
+          <button className='btn invite-btn'>
             Request Invite
           </button>
         </div>
