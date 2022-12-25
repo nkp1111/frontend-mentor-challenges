c245ef1bb597ab9811255e1fb52acef34dc9170e
neeraj <neerajp4321@gmail.com>
2022-12-25 20:06:27 +0530

    Chore: add live server

--- easybank-landing-page/src/main/index.js
+++ easybank-landing-page/src/main/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { mockupsImage } from '../assets/data'
 import BlogSection from './BlogSection'
 import FeatureSection from './FeatureSection'
