e66b39f1e004a25f438d942a219642c7eb624b7d
neeraj <neerajp4321@gmail.com>
2022-12-25 18:12:54 +0530

    Feat: add style to feature section

--- easybank-landing-page/src/main/FeatureSection.js
+++ easybank-landing-page/src/main/FeatureSection.js
@@ -3,10 +3,10 @@ import { featureArticleData } from '../assets/data'
 
 function FeatureSection() {
   return (
-    <section>
+    <section className='feature-section'>
       <h2>Why choose Easybank?</h2>
       <p>
-        We leverage Open Banking to turn your bank account into your financial hub. Control
+        We leverage Open Banking to turn your bank account into your financial hub.<br /> Control
         your finances like never before.
       </p>
 
@@ -23,14 +23,15 @@ function FeatureSection() {
 
 const FeatureArticle = ({ item }) => {
   return (
-    <article className='col-md-3'>
-      <div>
+    <article className='col-md-3 row feature-article'>
+      <div className='col-12 order-2'>
         <h3>{item.heading}</h3>
         <p>
           {item.description}
         </p>
       </div>
-      <div>
+
+      <div className='col-12 order-1'>
         <img src={item.image} alt={`${item.heading} logo`} />
       </div>
     </article>
