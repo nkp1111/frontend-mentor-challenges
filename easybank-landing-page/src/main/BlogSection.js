3dc1dd11828e50d97e89fb3fd62f132655cd1c5e
neeraj <neerajp4321@gmail.com>
2022-12-25 18:45:20 +0530

    Feat: add blog section styles

--- easybank-landing-page/src/main/BlogSection.js
+++ easybank-landing-page/src/main/BlogSection.js
@@ -3,25 +3,30 @@ import { blogArticleData } from '../assets/data'
 
 function BlogSection() {
   return (
-    <section>
+    <section className='blog-section'>
       <h2>Latest Articles</h2>
       <div className='row'>
         {blogArticleData.map(item => {
           return (
-            <article className='col-md-3 row' key={item.id}>
-              <div className='col order-2 row'>
+            <article
+              className='col-md-3 row blog-section-article' key={item.id}>
+              <div className='col-12 
+              order-2 
+              row 
+              blog-section-info'>
                 <div className='col-12 order-2'>
                   <h3>{item.heading}</h3>
                   <p>{item.description}</p>
                 </div>
 
                 <div className='col-12 order-1'>
-                  <p>{item.author}</p>
+                  <p className='blog-author'>{item.author}</p>
                 </div>
               </div>
 
-              <div className='col order-1'>
-                <img src={item.image} alt="article"></img>
+              <div className='col-12 order-1 blog-section-image'>
+                <img src={item.image} alt="article"
+                ></img>
               </div>
             </article>
           )
