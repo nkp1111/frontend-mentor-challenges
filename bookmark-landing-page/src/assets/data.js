26ff6bf6dcb35494e80855dedc54afabf5370911
nkp1111 <neerajp4321@gmail.com>
2023-01-01 10:11:19 +0530

    Feat: add images on data file, bootstrap

---
+++ bookmark-landing-page/src/assets/data.js
@@ -0,0 +1,24 @@
+import bgDotsImage from './images/bg-dots.svg'
+
+import iconArrowIcon from './images/icon-arrow.svg'
+import iconCloseIcon from './images/icon-close.svg'
+import iconErrorIcon from './images/icon-error.svg'
+import iconFacebookIcon from './images/icon-facebook.svg'
+import iconHamburgerIcon from './images/icon-hamburger.svg'
+import iconTwitterIcon from './images/icon-twitter.svg'
+
+import illustration1Image from './images/illustration-features-tab-1.svg'
+import illustration2Image from './images/illustration-features-tab-2.svg'
+import illustration3Image from './images/illustration-features-tab-3.svg'
+import illustrationHeroImage from './images/illustration-hero.svg'
+
+import logoBookmark from './images/logo-bookmark.svg'
+import logoChrome from './images/logo-chrome.svg'
+import logoFirefox from './images/logo-firefox.svg'
+import logoOpera from './images/logo-opera.svg'
+
+
+
+
+
+export { logoBookmark }
