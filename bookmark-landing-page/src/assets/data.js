be00e1e1d8dfb6b3ba60b8feb7a445f602c1d43e
nkp1111 <neerajp4321@gmail.com>
2023-01-01 10:33:00 +0530

    Feat: add header file

--- bookmark-landing-page/src/assets/data.js
+++ bookmark-landing-page/src/assets/data.js
@@ -1,11 +1,11 @@
 import bgDotsImage from './images/bg-dots.svg'
 
-import iconArrowIcon from './images/icon-arrow.svg'
-import iconCloseIcon from './images/icon-close.svg'
-import iconErrorIcon from './images/icon-error.svg'
-import iconFacebookIcon from './images/icon-facebook.svg'
-import iconHamburgerIcon from './images/icon-hamburger.svg'
-import iconTwitterIcon from './images/icon-twitter.svg'
+import iconArrow from './images/icon-arrow.svg'
+import iconClose from './images/icon-close.svg'
+import iconError from './images/icon-error.svg'
+import iconFacebook from './images/icon-facebook.svg'
+import iconHamburger from './images/icon-hamburger.svg'
+import iconTwitter from './images/icon-twitter.svg'
 
 import illustration1Image from './images/illustration-features-tab-1.svg'
 import illustration2Image from './images/illustration-features-tab-2.svg'
@@ -17,8 +17,15 @@ import logoChrome from './images/logo-chrome.svg'
 import logoFirefox from './images/logo-firefox.svg'
 import logoOpera from './images/logo-opera.svg'
 
+const navData = [
+  "feature", "pricing", "contact"
+]
 
 
 
-
-export { logoBookmark }
+export {
+  logoBookmark,
+  iconHamburger,
+  iconClose,
+  navData,
+}
---
+++ bookmark-landing-page/src/header/Index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react'
+import { logoBookmark, iconHamburger, iconClose, navData } from '../assets/data'
+
+function Index() {
+
+  return (
+    <header className='header'>
+      <nav className='navbar navbar-expand-md'>
+        <div>
+          <img src={logoBookmark} alt="logo bookmark" className='navbar-brand' />
+        </div>
+
+        <div
+          className='navbar-toggler'
+          data-bs-target="#headerNav"
+          data-bs-toggle="collapse">
+          <img src={iconHamburger} alt="hamburger-icon" ></img>
+        </div>
+
+        <div className='collapse navbar-collapse' id="headerNav">
+          <ul className='navbar-nav'>
+            {navData.map(item => {
+              return (
+                <li key={item} className='nav-items'>{item}</li>
+              )
+            })}
+          </ul>
+
+          <button className='btn'>
+            Login
+          </button>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Index
