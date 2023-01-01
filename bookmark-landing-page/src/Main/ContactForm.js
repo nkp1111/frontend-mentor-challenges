c4eea3a99630cf032a2bf5e12a739649e84a0cc4
nkp1111 <neerajp4321@gmail.com>
2023-01-01 19:01:04 +0530

    Feat: add contact form styles and error messages

--- bookmark-landing-page/src/Main/ContactForm.js
+++ bookmark-landing-page/src/Main/ContactForm.js
@@ -1,26 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { iconError } from '../assets/data'
 
 function ContactForm() {
+
+  let emailRef = useRef()
+
+  const setErrorStyle = () => {
+    // show error and remove after 2 second 
+    document.querySelector(".error-icon").style.display = "block"
+    emailRef.current.style.border = "2px solid var(--Soft-Red)"
+    document.querySelector(".error-message").style.display = "block"
+
+    setTimeout(function () {
+      document.querySelector(".error-icon").style.display = "none"
+      emailRef.current.style.border = "none"
+      document.querySelector(".error-message").style.display = "none"
+    }, 2000)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let inputValue = emailRef.current.value
+    let atPos = inputValue.lastIndexOf("@")
+    let dotPos = inputValue.lastIndexOf(".")
+    if (atPos === -1 || dotPos === -1 || atPos > dotPos) {
+      setErrorStyle()
+    }
+  }
+
   return (
     <section className='contact-form'>
-
-      <div className='container-fluid bg-dark'>
+      <div className='container-fluid'>
         <div className='row'>
           <h2 className='col-12'>Stay up-to-date with what we’re doing</h2>
           <p className='col-12'>35,000+ already joined</p>
-          <form className='col-12'>
+          <form className='col-12' onSubmit={(e) => handleSubmit(e)}>
             <div className='row'>
-              <div className='email-input-holder'>
-                <input type="email"
+              <div className='email-input-holder col-8'>
+                <input type="text"
                   name="email"
                   placeholder="Type your email here..."
-                  className='col-7' />
+                  ref={emailRef}
+                />
                 <span className='error-icon'>
                   <img src={iconError} alt="error icon"></img>
                 </span>
+                <span className='error-message'>
+                  Whoop's make sure it's an email
+                </span>
               </div>
-              <button className='btn contact-btn col-4'>Contact Us</button>
+              <button className='btn login-btn col-3'>Contact Us</button>
             </div>
           </form>
         </div>
