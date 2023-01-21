5e354d0b98cecc8a7a9986308f5afc61ad2b8f86
nkp1111 <neerajp4321@gmail.com>
2023-01-21 20:12:19 +0530

    Feat: add endpage

---
+++ manage-landing-page/src/main/EndPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function EndPage() {
+  return (
+    <section className='end-page'>
+      <h2>Simplify how your team works today.</h2>
+      <button className='btn btn-orange-white'>Get Started</button>
+    </section>
+  )
+}
+
+export default EndPage
