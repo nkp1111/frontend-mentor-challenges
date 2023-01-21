443e79faff9c209fff99c74a3445786ba4cd1fae
nkp1111 <neerajp4321@gmail.com>
2023-01-21 19:05:21 +0530

    Feat: add feature section content

---
+++ manage-landing-page/src/main/FeatureSection.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { featureSectionItems } from '../assets/data'
+
+function FeatureSection() {
+  return (
+    <section className='feature-section'>
+
+      <div className='head-portion'>
+        <h2>What’s different about Manage?</h2>
+        <p>
+          Manage provides all the functionality your team needs, without
+          the complexity. Our software is tailor-made for modern digital
+          product teams.
+        </p>
+      </div>
+
+      <div className='feature-portion'>
+        {featureSectionItems.map(item => {
+          return (
+            // articles
+            <article key={item.id}>
+              {/**flex direction row reverse */}
+              <div className='article-head d-flex'>
+                <h3>{item.title}</h3>
+                <span className='btn-orange'>0{item.id}</span>
+              </div>
+              <div className='article-body'>
+                <p>{item.description}</p>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+
+    </section>
+  )
+}
+
+export default FeatureSection
--- manage-landing-page/src/main/index.js
+++ manage-landing-page/src/main/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import FeatureSection from './FeatureSection'
 import HomePage from './HomePage'
 
 function index() {
   return (
-    <main>
+    <main className='container-fluid'>
       <HomePage />
+      <FeatureSection />
     </main>
   )
 }
