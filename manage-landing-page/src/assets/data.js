91b4661d0cf2c1325dad3b9c4a187ff25fdff8b8
nkp1111 <neerajp4321@gmail.com>
2023-01-21 18:12:02 +0530

    Feat: add bootstrap, css reset and data file

---
+++ manage-landing-page/src/assets/data.js
@@ -0,0 +1,24 @@
+// avatars 
+import avatarAli from './images/avatar-ali.png'
+import avatarAnisha from './images/avatar-anisha.png'
+import avatarRichard from './images/avatar-richard.png'
+import avatarShanai from './images/avatar-shanai.png'
+// icons 
+import iconClose from './images/icon-close.svg'
+import iconFacebook from './images/icon-facebook.svg'
+import iconHamburger from './images/icon-hamburger.svg'
+import iconInstagram from './images/icon-instagram.svg'
+import iconPinterest from './images/icon-pinterest.svg'
+import iconTwitter from './images/icon-twitter.svg'
+import iconYoutube from './images/icon-youtube.svg'
+// background image 
+import bgDesktop from './images/bg-simplify-section-desktop.svg'
+import bgMobile from './images/bg-simplify-section-mobile.svg'
+import bgTablet from './images/bg-tablet-pattern.svg'
+// logo
+import logo from './images/logo.svg'
+
+
+export {
+  logo
+}
