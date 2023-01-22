6a8e8b051591c69bc6167bfbb2b5a15ec3d7f25a
nkp1111 <neerajp4321@gmail.com>
2023-01-22 12:54:57 +0530

    Feat: add feature section styles

--- manage-landing-page/src/main/FeatureSection.js
+++ manage-landing-page/src/main/FeatureSection.js
@@ -6,7 +6,7 @@ function FeatureSection() {
     <section className='feature-section'>
 
       <div className='head-portion'>
-        <h2>What’s different about Manage?</h2>
+        <h2 className='section-heading'>What’s different about Manage?</h2>
         <p>
           Manage provides all the functionality your team needs, without
           the complexity. Our software is tailor-made for modern digital
@@ -21,7 +21,7 @@ function FeatureSection() {
             <article key={item.id}>
               {/**flex direction row reverse */}
               <div className='article-head d-flex'>
-                <h3>{item.title}</h3>
+                <h3 className='article-heading'>{item.title}</h3>
                 <span className='btn-orange'>0{item.id}</span>
               </div>
               <div className='article-body'>
