83349eb709da1af32a0527c7095cae262b8ea67e
nkp1111 <neerajp4321@gmail.com>
2023-01-22 14:31:17 +0530

    Feat: add testimonial section styles

--- manage-landing-page/src/main/TestimonialSection.js
+++ manage-landing-page/src/main/TestimonialSection.js
@@ -6,7 +6,7 @@ function TestimonialSection() {
   useEffect(() => {
     // change carousel article after certain time
     let current = 0
-    let carouselChangeTime = 5000
+    let carouselChangeTime = 10000
     let carouselItem = document.querySelectorAll(".carousel-item")
 
     const removeActive = () => {
@@ -14,7 +14,7 @@ function TestimonialSection() {
       carouselItem.forEach(item => item.classList.remove("active"))
     }
 
-    setInterval(() => {
+    let interval = setInterval(() => {
       removeActive()
       carouselItem[current].classList.add("active")
       if (current < carouselItem.length - 1) {
@@ -28,7 +28,7 @@ function TestimonialSection() {
 
   return (
     <section className='testimonial-section'>
-      <h2>What they’ve said</h2>
+      <h2 className="section-heading">What they’ve said</h2>
 
       <div id="test-carousel" className='carousel slide'>
         <div className='carousel-inner'>
@@ -37,7 +37,7 @@ function TestimonialSection() {
               <div key={item.id}
                 className="carousel-item">
                 <div className='carousel-info'>
-                  <h3>{item.user}</h3>
+                  <h3 className='article-heading'>{item.user}</h3>
                   <p>{item.review}</p>
                 </div>
                 <div className='carousel-img'>
@@ -49,7 +49,7 @@ function TestimonialSection() {
         </div>
       </div>
 
-      <div className='carousel'>
+      <div className='carousel c-2'>
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#test-carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#test-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -58,7 +58,9 @@ function TestimonialSection() {
         </div>
       </div>
 
-      <button className='btn btn-orange'>Get Started</button>
+      <div className='btn-holder'>
+        <button className='btn btn-orange'>Get Started</button>
+      </div>
 
     </section>
   )
