e3caf8a4712f7f55776d2c1e7b980d260c264d50
nkp1111 <neerajp4321@gmail.com>
2023-01-28 17:51:25 +0530

    Feat: add frontinfo file and data file

---
+++ social-media-dashboard/src/assets/data.js
@@ -0,0 +1,48 @@
+import downIcon from './images/icon-down.svg'
+import facebookIcon from './images/icon-facebook.svg'
+import instagramIcon from './images/icon-instagram.svg'
+import twitterIcon from './images/icon-twitter.svg'
+import upIcon from './images/icon-up.svg'
+import youtubeIcon from './images/icon-youtube.svg'
+
+
+let dashboardCurrentInfo = [
+  {
+    id: 1,
+    socialPlatform: "facebook",
+    author: "@nathanf",
+    count: "1987",
+    countOfToday: "12",
+    icon: facebookIcon,
+  },
+  {
+    id: 2,
+    socialPlatform: "facebook",
+    author: "@nathanf",
+    count: "1044",
+    countOfToday: "99",
+    icon: twitterIcon,
+  },
+  {
+    id: 3,
+    socialPlatform: "facebook",
+    author: "@realnathanf",
+    count: "11K",
+    countOfToday: "1099",
+    icon: instagramIcon,
+  },
+  {
+    id: 4,
+    socialPlatform: "facebook",
+    author: "Nathan F.",
+    count: "8239",
+    countOfToday: "144",
+    icon: youtubeIcon,
+  },
+]
+
+export {
+  dashboardCurrentInfo,
+  upIcon,
+  downIcon,
+}
---
+++ social-media-dashboard/src/components/FrontInfo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function FrontInfo() {
+  return (
+    <div className='front-info row'>
+      <div className='col-md-7'>
+        <h1 className='main-heading'>Social Media Dashboard</h1>
+        <p>
+          Total Followers: 23,004
+        </p>
+      </div>
+      <div className='switch col-md-5'>
+        <span className='ball'></span>
+      </div>
+      <div className='divider'></div>
+    </div>
+  )
+}
+
+export default FrontInfo
