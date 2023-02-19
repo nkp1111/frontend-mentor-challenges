1236ebc481046eccac3bf91caada9f8176e5b172
nkp1111 <neerajp4321@gmail.com>
2023-02-19 10:09:17 +0530

    Feat: add initial styles and footer

---
+++ result-summary-component/src/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function index() {
+  return (
+    <footer className='attribution'>
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/nkp1111">Your Name Here</a>.
+    </footer>
+  )
+}
+
+export default index
