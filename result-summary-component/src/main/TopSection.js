c5116caabba48e9d1f0657dfae576b7b2a4e6173
nkp1111 <neerajp4321@gmail.com>
2023-02-19 12:05:09 +0530

    Feat: add topSection div  styles

--- result-summary-component/src/main/TopSection.js
+++ result-summary-component/src/main/TopSection.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function TopSection() {
   return (
-    <div className='col-md-6'>
+    <div className='col-md-6 front-page'>
       <h1 className='main-heading'>Your Result</h1>
-      <div className='score-board'>
+      <div className='final-score d-flex flex-column justify-content-around'>
         <span>76</span>
         <p>of 100</p>
       </div>
-      <div className='congrats-msg-box'>
+      <div className='msg-box'>
         <span>Great</span>
         <p>
           You scored higher than 65% of the people who have taken these tests.
