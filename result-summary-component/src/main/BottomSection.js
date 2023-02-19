5c011bf591643753688dc06d906c1b90fc328726
nkp1111 <neerajp4321@gmail.com>
2023-02-19 12:31:56 +0530

    Feat: add media query and different background

--- result-summary-component/src/main/BottomSection.js
+++ result-summary-component/src/main/BottomSection.js
@@ -7,9 +7,10 @@ function BottomSection() {
       <h2 className='section-heading'>Summary</h2>
       {data.map(item => {
         return (
-          <div className='card d-flex flex-row align-items-center'>
+          <div key={item.category}
+            className={`card d-flex flex-row align-items-center cat-${item.category.toLowerCase()}`}>
             <img src={item.icon} alt="icon" />
-            <p className={`category cat-${item.category.toLowerCase()}`}>{item.category}</p>
+            <p className='category'>{item.category}</p>
             <p className='score ms-auto'>{item.score}</p>
           </div>
         )
