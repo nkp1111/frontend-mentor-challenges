32bc48dae8dc082f6d2e7f9e1294ab5e55ab0eca
nkp1111 <neerajp4321@gmail.com>
2023-02-19 11:10:41 +0530

    Feat: add bottom section content

---
+++ result-summary-component/src/assets/data.js
@@ -0,0 +1,31 @@
+import iconMemory from './images/icon-memory.svg'
+import iconReaction from './images/icon-reaction.svg'
+import iconVerbal from './images/icon-verbal.svg'
+import iconVisual from './images/icon-visual.svg'
+
+const data = [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": iconReaction,
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": iconMemory,
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": iconVerbal,
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": iconVisual,
+  }
+]
+
+export {
+  data
+}
--- result-summary-component/src/main/BottomSection.js
+++ result-summary-component/src/main/BottomSection.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { data } from '../assets/data'
 
 function BottomSection() {
   return (
-    <div className='col-md-6'>
-
-    </div>
+    <section className='col-md-6'>
+      <h2>Summary</h2>
+      {data.map(item => {
+        return (
+          <div className='card d-flex'>
+            <img src={item.icon} alt="icon" />
+            <p className='category'>{item.category}</p>
+            <p className='score'>{item.score}</p>
+          </div>
+        )
+      })}
+    </section>
   )
 }
 
