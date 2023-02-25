dc24170b68152d53b4908b1afc86a451585dbf30
nkp1111 <neerajp4321@gmail.com>
2023-02-25 16:14:18 +0530

    Feat: add styles to card body

--- chat-app-illustration/src/main/ChatView.js
+++ chat-app-illustration/src/main/ChatView.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function ChatView({ chatData }) {
   return (
-    <div className="card-body">
+    <div className="card-body d-flex flex-column">
       {chatData.map(item => {
         return (
-          <div className="content" key={item.id}>
+          <div className={`content msg-${item.action} type-${item.type}`} key={item.id}>
             {item.type === "img"
               // image message 
               ? <ImageMessage item={item} />
@@ -23,7 +23,7 @@ function ChatView({ chatData }) {
 
 const ImageMessage = ({ item }) => {
   return (
-    <div className="img-holder">
+    <div className="img-holder d-flex">
       {item.message.map((item, ind) => {
         return (
           <img src={item} alt="." key={ind}></img>
@@ -36,8 +36,15 @@ const ImageMessage = ({ item }) => {
 const BusinessMessage = ({ item }) => {
   return (
     <div className="business d-flex align-items-center">
-      <input type="radio" value={item.price}></input>
-      <p>{item.message} <span>${item.price}</span></p>
+      <div className="radio-holder">
+        <input
+          type="radio"
+          value={item.price}
+          name="price"
+          id={`plan-${item.id}`}></input>
+      </div>
+      <label htmlFor={`plan-${item.id}`}>{item.message}</label>
+      <span className='price ms-auto'>${item.price}</span>
     </div>
   )
 }
