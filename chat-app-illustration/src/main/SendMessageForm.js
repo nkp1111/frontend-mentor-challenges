0a73e20ca89b75b74458a4b681e30b4445b3ebc9
nkp1111 <neerajp4321@gmail.com>
2023-02-25 16:31:37 +0530

    Feat: add style to card footer

--- chat-app-illustration/src/main/SendMessageForm.js
+++ chat-app-illustration/src/main/SendMessageForm.js
@@ -3,11 +3,11 @@ import { FaChevronRight } from '../assets/data'
 
 function SendMessage() {
   return (
-    <div className='card-footer'>
+    <div className='card-foot'>
       <form action="#">
         <input type="text" name="text" placeholder='Type a message…' />
         <button aria-label='button' type="submit">
-          <FaChevronRight />
+          <FaChevronRight className='submit-btn-icon' />
         </button>
       </form>
     </div>
