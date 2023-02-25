e5a2e171851112f4789d84e4ab51aa634beacd9c
nkp1111 <neerajp4321@gmail.com>
2023-02-25 11:41:58 +0530

    Chore: add react icons and data file

---
+++ chat-app-illustration/src/assets/data.js
@@ -0,0 +1,18 @@
+import avatarImg from './images/avatar.jpg'
+import dogImg1 from './images/dog-image-1.jpg'
+import dogImg2 from './images/dog-image-2.jpg'
+import dogImg3 from './images/dog-image-3.jpg'
+import { BsChevronLeft } from 'react-icons/bs'
+import { SlOptionsVertical } from 'react-icons/sl'
+
+
+const chatData = [
+
+]
+
+export {
+  avatarImg,
+  chatData,
+  BsChevronLeft,
+  SlOptionsVertical,
+}
