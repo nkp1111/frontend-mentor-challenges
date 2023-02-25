c40f4ca97b8a0f4ae4db388d6138e7cdc41a380f
nkp1111 <neerajp4321@gmail.com>
2023-02-25 22:14:25 +0530

    Feat: add media query and change js for big width

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -5,8 +5,17 @@ const shareDiv = document.querySelector(".share-div")
 
 shareBtnS.forEach(btn => {
   btn.addEventListener("click", (e) => {
-    authorDiv.classList.toggle("show")
-    authorDiv.classList.toggle("hide")
+    if (window.innerWidth < 768) {
+      authorDiv.classList.toggle("show")
+      authorDiv.classList.toggle("hide")
+    } else {
+      shareBtnS[0].classList.toggle("active")
+      if (shareBtnS[0].classList.contains("active")) {
+        shareBtnS[0].querySelector("img").src = "./images/icon-share-white.svg"
+      } else {
+        shareBtnS[0].querySelector("img").src = "./images/icon-share.svg"
+      }
+    }
     shareDiv.classList.toggle("show")
     shareDiv.classList.toggle("hide")
   })
