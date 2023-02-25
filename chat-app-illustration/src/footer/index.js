39023e5262436f547161aaf40970f86cba19523f
nkp1111 <neerajp4321@gmail.com>
2023-02-25 10:58:21 +0530

    Feat: add main content and footer

---
+++ chat-app-illustration/src/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./footer.css"
+
+function index() {
+  return (
+    <footer className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/nkp1111">Neeraj Parmar</a>.
+    </footer>
+  )
+}
+
+export default index
---
+++ chat-app-illustration/src/main/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function index() {
+  return (
+    <main>
+      <h1> Simple booking</h1>
+      <p>
+        Stay in touch with our dog walkers through the chat interface. This makes it easy to
+        discuss arrangements and make bookings. Once the walk has been completed you can rate
+        your walker and book again all through the chat.
+      </p>
+    </main>
+  )
+}
+
+export default index
