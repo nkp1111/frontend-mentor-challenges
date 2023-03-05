a7a2925eb3fc093d9fa611b6f21e6d9e398c087d
nkp1111 <neerajp4321@gmail.com>
2023-03-05 13:09:45 +0530

    Feat: add hamburger icon change

--- project-tracking-intro-component/src/header/NavbarTogglerBtn.js
+++ project-tracking-intro-component/src/header/NavbarTogglerBtn.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function NavbarTogglerBtn({ iconClose, iconHamburger }) {
+  const [currentToggleImage, setCurrentToggleImage] = useState(iconHamburger)
+
+  useEffect(() => {
+    // change icons on click btw hamburger and close icon
+    if (iconClose && iconHamburger) {
+      const toggler = document.querySelector(".navbar-toggler")
+      toggler.addEventListener("click", () => {
+        if (toggler.classList.contains("collapsed")) {
+          setCurrentToggleImage(iconHamburger)
+        } else {
+          setCurrentToggleImage(iconClose)
+        }
+      })
+    }
+  })
+
   return (
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
       {iconClose && iconHamburger
-        ? <img src={iconHamburger} alt="hamburger-icon" className='nav-hamburger-icon' />
+        ? <img src={currentToggleImage} alt="hamburger-icon" className='nav-hamburger-icon' />
         : <span className="navbar-toggler-icon nav-hamburger-icon"></span>}
     </button>
   )
