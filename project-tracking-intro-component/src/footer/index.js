f6ec4d67f67becb10ebd33dde53e5bfac1151931
nkp1111 <neerajp4321@gmail.com>
2023-03-05 11:41:41 +0530

    Feat: add footer

---
+++ project-tracking-intro-component/src/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./footer.css"
+
+function index() {
+  return (
+    <footer>
+      <p className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/nkp1111">Neeraj Parmar</a>.
+      </p>
+    </footer>
+  )
+}
+
+export default index
