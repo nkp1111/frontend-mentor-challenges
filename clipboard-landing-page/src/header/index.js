ff944fe2f2e28273b45a8ca55cfd0bf1ec548963
nkp1111 <neerajp4321@gmail.com>
2023-03-11 16:34:21 +0530

    Feat: add style to header

--- clipboard-landing-page/src/header/index.js
+++ clipboard-landing-page/src/header/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { headerLogoDesktop, headerLogoMobile, logo } from '../assets/data'
+import "./header.css"
 
 function index() {
   return (
     <header>
+      <img src={logo} alt="logo" className='logo' />
       <div className="img-holder">
-        <img src={logo} alt="logo" className='logo' />
         <picture>
           <source srcSet={headerLogoMobile} media="(max-width: 48rem)" />
           <img src={headerLogoDesktop} alt="header background" />
