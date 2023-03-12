f271f85da8c4c36316bdcb2e6aff7d1284ed23a1
nkp1111 <neerajp4321@gmail.com>
2023-03-12 10:29:28 +0530

    Feat: add front page content

---
+++ huddle-landing-page-with-alternating-feature-blocks/src/main/FrontPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { illustrationMockups } from '../assets/data'
+
+function FrontPage() {
+  return (
+    <div className='front-page'>
+      <div className="row">
+        <div className="col-md-6">
+          {/* front page info  */}
+          <h1 className='main-heading'>Build The Community Your Fans Will Love</h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+            Create connections with your users as you engage in genuine discussion.
+          </p>
+          <button className="btn huddle-btn">Get Started For Free</button>
+        </div>
+        <div className="col-md-6">
+          {/* front page image */}
+          <img src={illustrationMockups} alt="." />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FrontPage
---
+++ huddle-landing-page-with-alternating-feature-blocks/src/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FrontPage from './FrontPage'
+
+function index() {
+  return (
+    <main>
+      <div className="container-fluid">
+        <FrontPage />
+      </div>
+    </main>
+  )
+}
+
+export default index
