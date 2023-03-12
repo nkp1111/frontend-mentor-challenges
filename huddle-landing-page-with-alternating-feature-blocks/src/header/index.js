a47b6fc818cdff2265f862be618e3fd31e5bc17f
nkp1111 <neerajp4321@gmail.com>
2023-03-12 10:18:14 +0530

    Feat: add header content

---
+++ huddle-landing-page-with-alternating-feature-blocks/src/header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function index() {
+  return (
+    <header>
+      <div className="container-fluid">
+        <nav className="navbar navbar-expand-sm">
+          <div className="nav-logo">
+            <img src="" alt="" className='navbar-brand' />
+          </div>
+          <div className="btn-holder ms-auto">
+            <button className="btn">Try It Free</button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default index
