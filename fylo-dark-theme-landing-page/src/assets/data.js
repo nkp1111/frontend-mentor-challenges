669b6d54560e00e27e927116272d1cd702eb5828
nkp1111 <neerajp4321@gmail.com>
2023-03-12 18:16:43 +0530

    Feat: add header and logo component

---
+++ fylo-dark-theme-landing-page/src/assets/data.js
@@ -0,0 +1,24 @@
+import logo from './images/logo.svg'
+
+const navbarLinks = [
+  {
+    id: 1,
+    title: "Features",
+    link: "",
+  },
+  {
+    id: 1,
+    title: "Team",
+    link: "",
+  },
+  {
+    id: 1,
+    title: "Sign In",
+    link: "",
+  },
+]
+
+export {
+  logo,
+  navbarLinks,
+}
