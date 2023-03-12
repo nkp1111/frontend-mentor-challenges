78ad869e3d0f46e57dfb412c596c84de419f9aab
nkp1111 <neerajp4321@gmail.com>
2023-03-12 14:22:33 +0530

    Feat: add styles to main's feature section

--- huddle-landing-page-with-alternating-feature-blocks/src/main/FeatureSection.js
+++ huddle-landing-page-with-alternating-feature-blocks/src/main/FeatureSection.js
@@ -4,7 +4,7 @@ import { featureSectionArticles } from '../assets/data'
 function FeatureSection() {
   return (
     <section className='feature-section'>
-      <div className="floating">
+      <div className="floating d-flex flex-column align-items-center justify-content-evenly">
         <h2 className='section-heading'>Ready To Build Your Community?</h2>
         <button className="btn huddle-btn">Get Started For Free</button>
       </div>
@@ -13,7 +13,7 @@ function FeatureSection() {
       <div className='article-holder'>
         {featureSectionArticles.map((item, ind) => {
           return (
-            <article className='row' key={item.id}>
+            <article className='row align-items-center' key={item.id}>
               <div className={`col-md-6 
               ${ind % 2 !== 0 ? "order-2" : ""}`}>
                 <h3 className='article-heading'>{item.title}</h3>
