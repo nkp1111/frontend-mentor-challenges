7f2f359ba1c66dffba5ef7935367a0326544cce0
nkp1111 <neerajp4321@gmail.com>
2023-03-18 11:13:44 +0530

    Feat: add logo component and data file

---
+++ fylo-landing-page-with-two-column-layout/src/assets/data.js
@@ -0,0 +1,25 @@
+import logo from './images/logo.svg'
+
+const navbarLinks = [
+  {
+    id: 1,
+    title: "Features",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Team",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Sign In",
+    link: "#",
+  },
+]
+
+
+export {
+  logo,
+  navbarLinks,
+}
---
+++ fylo-landing-page-with-two-column-layout/src/component/logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { logo } from '../assets/data'
+
+function Fylologo() {
+  return (
+    <a className="navbar-brand" href="#">
+      <img src={logo} alt="logo" />
+    </a>
+  )
+}
+
+export default Fylologo
