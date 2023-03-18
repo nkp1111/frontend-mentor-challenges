afa336f43366281df93011d97d224f9475371e30
nkp1111 <neerajp4321@gmail.com>
2023-03-18 15:40:56 +0530

    Feat: add styles to footer and media query

--- fylo-landing-page-with-two-column-layout/src/footer/index.js
+++ fylo-landing-page-with-two-column-layout/src/footer/index.js
@@ -3,25 +3,24 @@ import Logo from '../component/logo'
 import ContactInfo from './ContactInfo'
 import FooterNavLinks from './FooterNavLinks'
 import SocialLinks from './SocialLinks'
+import "./footer.css"
 
 function index() {
   return (
     <footer>
       <div className="container-fluid">
-        <Logo />
+        <Logo color="white" />
         <div className="row align-items-start">
           {/* contact icons  */}
           <div className="col-md-5 contacts">
             <ContactInfo />
           </div>
           {/* nav links  */}
-          <div className="col-md-5 nav-links">
-            <div className="container">
-              <FooterNavLinks />
-            </div>
+          <div className="col-md-4 nav-links">
+            <FooterNavLinks />
           </div>
           {/* social links  */}
-          <div className="col-md-2 social-links">
+          <div className="col-md-3 social-links">
             <SocialLinks />
           </div>
         </div>
