61702907e6cb73b5c5125659bb67d6373fbb7836
nkp1111 <neerajp4321@gmail.com>
2023-03-18 18:33:18 +0530

    Fix: add different label and id for multiple forms

--- fylo-landing-page-with-two-column-layout/src/component/form.js
+++ fylo-landing-page-with-two-column-layout/src/component/form.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import emailValidation from './emailValidation'
 
 function Form() {
@@ -7,10 +7,19 @@ function Form() {
     emailValidation()
   })
 
+  useEffect(() => {
+    const labels = document.querySelectorAll("label")
+    const inputs = document.querySelectorAll("label+input")
+    labels.forEach((label, ind) => {
+      label.htmlFor = "email-" + ind
+      inputs[ind].id = "email-" + ind
+    })
+  })
+
   return (
     <form className='d-flex needs-validation' noValidate>
       <div>
-        <label htmlFor="email" className="form-label visually-hidden">Email</label>
+        <label htmlFor='email' className="form-label visually-hidden">Email</label>
         <input type="email" placeholder='Enter your email' className='form-control' id="email" />
         <div className="invalid-feedback">
           Looks good!
