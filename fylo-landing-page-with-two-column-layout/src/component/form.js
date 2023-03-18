b1dbd19d50b1086f9e87cdf7d145a28b11497207
nkp1111 <neerajp4321@gmail.com>
2023-03-18 11:31:04 +0530

    Feat: add section 1 form and bootstrap validation

---
+++ fylo-landing-page-with-two-column-layout/src/component/form.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import emailValidation from './emailValidation'
+
+function Form() {
+
+  useEffect(() => {
+    emailValidation()
+  })
+
+  return (
+    <form className='d-flex needs-validation' noValidate>
+      <div>
+        <label htmlFor="email" className="form-label visually-hidden">Email</label>
+        <input type="email" placeholder='Enter your email' className='form-control' />
+        <div className="invalid-feedback">
+          Looks good!
+        </div>
+      </div>
+      <div>
+        <button className="btn">
+          Get Started
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default Form
